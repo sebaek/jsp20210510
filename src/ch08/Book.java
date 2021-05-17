3aff82a1c1610dd79bdeea460240f8fb403440f5
sebaek <sebaek@gmail.com>
2021-05-17 11:15:04 +0900

    java bean, property

---
+++ src/ch08/Book.java
@@ -0,0 +1,43 @@
+package ch08;
+
+public class Book {
+	private String code;
+	private String title;
+	private String writer;
+	private String publisher;
+	private int price;
+	
+	public String getBarCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+}
