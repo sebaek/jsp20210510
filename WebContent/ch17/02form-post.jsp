2597a0bc465815a2aeeb50edc740dfc175cb085c
sebaek <sebaek@gmail.com>
2021-05-25 11:39:06 +0900

    post

---
+++ WebContent/ch17/02form-post.jsp
@@ -0,0 +1,37 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ page import="java.util.*" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<%@ taglib prefix="my" tagdir="/WEB-INF/tags/ch16/lecture" %>
+<%@ taglib prefix="tf" tagdir="/WEB-INF/tags/ch16/textbook" %>
+
+<% request.setCharacterEncoding("utf-8"); %>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<%@ include file="/WEB-INF/subModules/bootstrapHeader.jsp" %>
+
+<title>Insert title here</title>
+</head>
+<body>
+<div class="container">
+	<form action="<%= request.getContextPath() %>/Servlet06Post" method="post">
+		이름 : <input type="text" name="name" />
+		<br>
+		주소 : <input type="text" name="address" />
+		<br>
+		<input type="submit" />
+	</form>
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
---
+++ src/ch17/lecture/Servlet06Post.java
@@ -0,0 +1,50 @@
+package ch17.lecture;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet06Post
+ */
+@WebServlet("/Servlet06Post")
+public class Servlet06Post extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet06Post() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("servlet06 doPost method......");
+		
+		String name = request.getParameter("name");
+		String address = request.getParameter("address");
+		
+		System.out.println(name);
+		System.out.println(address);
+	}
+
+}
+
+
+
+
