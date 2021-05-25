9d7f3428fbb1756a74135ed9711a75ec9fc0d03e
sebaek <sebaek@gmail.com>
2021-05-25 09:40:35 +0900

    servlet

---
+++ src/ch17/lecture/Servlet02.java
@@ -0,0 +1,61 @@
+package ch17.lecture;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Servlet02
+ */
+@WebServlet("/Servlet02")
+public class Servlet02 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet02() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("servlet02 doget method....");
+		
+		//jsp 8개 기본객체
+		// request, response
+		// session, application
+		// page, config
+		// out, pageContext
+		
+		/* servlet에서 기본객체 얻기 */
+		HttpSession session = request.getSession();
+		ServletContext application = request.getServletContext();
+		ServletConfig config = getServletConfig();
+		PrintWriter out = response.getWriter();
+
+		out.print("<h1>");
+		out.print("hello servlet");
+		out.print("</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
