9cd30ab7475ff0ec8206e2662d54bb42cdbd9b4c
sebaek <sebaek@gmail.com>
2021-05-25 16:25:18 +0900

    servletcontext initilized

--- src/ch20/AppListener01.java
+++ src/ch20/AppListener01.java
@@ -1,14 +1,14 @@
 package ch20;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
 
 /**
  * Application Lifecycle Listener implementation class AppListener01
  *
  */
-@WebListener
+//@WebListener
 public class AppListener01 implements ServletContextListener {
 
     /**
@@ -30,6 +30,14 @@ public class AppListener01 implements ServletContextListener {
      */
     public void contextInitialized(ServletContextEvent sce)  { 
          System.out.println("어플리케이션 초기화 되었습니다.");
+         ServletContext application = sce.getServletContext();
+         String driver = application.getInitParameter("jdbcdriver");
+         String url = application.getInitParameter("jdbcUrl");
+         
+         System.out.println(driver);
+         System.out.println(url);
+         
+         System.out.println("초기화 메소드 종료");
     }
 	
 }
