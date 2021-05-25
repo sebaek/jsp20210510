15ff29ed64d857f2467a00e125446c5fd39c2640
sebaek <sebaek@gmail.com>
2021-05-25 10:19:31 +0900

    web.xml

--- src/ch17/lecture/Servlet03.java
+++ src/ch17/lecture/Servlet03.java
@@ -1,9 +1,6 @@
-package ch17.textbook;
+package ch17.lecture;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Date;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class NowServlet
+ * Servlet implementation class Servlet03
  */
-//@WebServlet("/NowServlet")
-public class NowServlet extends HttpServlet {
+//@WebServlet("/Servlet03")
+public class Servlet03 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public NowServlet() {
+    public Servlet03() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -29,16 +26,8 @@ public class NowServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html; charset=utf-8");
+		System.out.println("servlet 03 doGet ......");
 		
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<head><title>현재시간</title></head>");
-		out.println("<body>");
-		out.println("현재시간은");
-		out.println(new Date());
-		out.println("입니다.");
-		out.println("</body></html>");
 	}
 
 	/**
