ab25e1a0ba15cd3b3230856d3879086d97114f9f
sebaek <sebaek@gmail.com>
2021-05-25 15:15:58 +0900

    init

---
+++ src/ch17/textbook/DBCPInit2.java
@@ -0,0 +1,31 @@
+package ch17.textbook;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DBCPInit2
+ */
+//@WebServlet("/DBCPInit2")
+public class DBCPInit2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    @Override
+    public void init() throws ServletException {
+//    	loadJDBCDriver();
+//    	initConnectionPool();
+    	
+    	
+    }
+
+}
+
+
+
+
+
+
