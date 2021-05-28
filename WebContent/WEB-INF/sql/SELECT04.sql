614adc7cfd7ebc97627f91667247d6ef5f7e633f
sebaek <sebaek@gmail.com>
2021-05-28 11:38:18 +0900

    rs.next

--- WebContent/WEB-INF/sql/SELECT04.sql
+++ WebContent/WEB-INF/sql/SELECT04.sql
@@ -1,3 +1,24 @@
 SELECT CustomerName FROM Customers
 WHERE CustomerID = 1;
 
+SELECT CustomerName, City
+FROM Customers
+WHERE CustomerID = 1;
+
+SELECT CustomerID, CustomerName, City
+FROM Customers
+WHERE CustomerID = 2;
+
+SELECT CustomerID
+FROM Customers;
+
+SELECT * FROM Employees
+WHERE EmployeeID = 1;
+
+SELECT DISTINCT City FROM Customers;
+
+
+
+
+
+
