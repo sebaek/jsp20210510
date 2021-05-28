ff0483e4b5b7d566935c75229653432e920164f5
sebaek <sebaek@gmail.com>
2021-05-28 10:44:40 +0900

    jdbc2

--- src/ch14/JDBC02Servlet.java
+++ src/ch14/JDBC02Servlet.java
@@ -1,6 +1,12 @@
 package ch14;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,8 +32,74 @@ public class JDBC02Servlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		executeJDBC();
+		response.getWriter().print("<h1>jdbc02</h1>");
+	}
+	
+	private void executeJDBC() {
+
+		String sql = "SELECT CustomerName FROM Customers WHERE CustomerID = 1";
+
+		String url = "jdbc:mysql://13.125.118.27/test"; // 본인 ip
+		String user = "root";
+		String password = "wnddkdwjdqhcjfl1";
+
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			// 클래스 로딩
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			// 연결
+			con = DriverManager.getConnection(url, user, password);
+
+			// statement 생성
+			stmt = con.createStatement();
+
+			// 쿼리 실행, 결과(ResultSet) 리턴
+			rs = stmt.executeQuery(sql);
+
+			// 결과 탐색
+			if (rs.next()) {
+				String name = rs.getString(1);
+				System.out.println(name);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 연결 닫기
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+
 	}
 
 	/**
