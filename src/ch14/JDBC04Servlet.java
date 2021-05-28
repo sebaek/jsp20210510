f6f05fe7e35594347abd7f02557b63fdbd169b76
sebaek <sebaek@gmail.com>
2021-05-28 11:27:04 +0900

    jdbc04

--- src/ch14/JDBC04Servlet.java
+++ src/ch14/JDBC04Servlet.java
@@ -1,6 +1,13 @@
 package ch14;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,8 +33,88 @@ public class JDBC04Servlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		executeJDBC();
+		response.getWriter().print("<h1>jdbc04</h1>");
+	}
+	
+	private void executeJDBC() {
+
+		String sql = "SELECT * "
+				+ "FROM Employees "
+				+ "WHERE EmployeeID = 1";
+
+		String url = "jdbc:mysql://13.125.118.27/test"; // 본인 ip
+		String user = "root";
+		String password = "wnddkdwjdqhcjfl1";
+
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			// 클래스 로딩
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			// 연결
+			con = DriverManager.getConnection(url, user, password);
+
+			// statement 생성
+			stmt = con.createStatement();
+
+			// 쿼리 실행, 결과(ResultSet) 리턴
+			rs = stmt.executeQuery(sql);
+
+			// 결과 탐색
+			if (rs.next()) {
+//				int id = Integer.parseInt(rs.getString(1));
+				int id = rs.getInt(1);
+				String lastName = rs.getString(2);
+				String firstName = rs.getString(3);
+//				String bDate = rs.getString(4);
+				Date bDate = rs.getDate(4);
+				String photo = rs.getString(5);
+				String note = rs.getString(6);
+				
+				System.out.println(id);
+				System.out.println(lastName);
+				System.out.println(firstName);
+				System.out.println(bDate);
+				System.out.println(photo);
+				System.out.println(note);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 연결 닫기
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+
 	}
 
 	/**
