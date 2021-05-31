397ccaf21ba52b9b8c463be789bce990eaddd401
sebaek <sebaek@gmail.com>
2021-05-31 14:09:36 +0900

    insert into

---
+++ WebContent/WEB-INF/sql/INSERT01.sql
@@ -0,0 +1,10 @@
+SELECT * FROM Customers
+ORDER BY CustomerID DESC;
+
+INSERT INTO Customers
+(CustomerID, CustomerName,
+ ContactName, Address,
+ City, PostalCode, Country)
+VALUES
+(92, 'Kim', 'Lee', 'Gangnam', 'Seoul', '00000', 'Korea');
+
