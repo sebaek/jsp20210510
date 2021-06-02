3c39dd79694d11b09b53c490fc7e7ebead26ce29
sebaek <sebaek@gmail.com>
2021-06-02 10:18:55 +0900

    numeric

--- WebContent/WEB-INF/sql/CREATE_TABLE01.sql
+++ WebContent/WEB-INF/sql/CREATE_TABLE01.sql
@@ -16,3 +16,18 @@ WHERE Col1 = 999;
 UPDATE MyTable1
 SET Col1 = 777
 WHERE Col1 = 888;
+
+CREATE TABLE MyTable2
+(
+	Col1 INT,
+    Col2 INT
+);
+
+DESC MyTable2;
+INSERT INTO MyTable2
+(Col1, Col2)
+VALUES 
+(987, 123);
+SELECT * FROM MyTable2;
+
+
---
+++ WebContent/WEB-INF/sql/CREATE_TABLE02_DATA_TYPE.sql
@@ -0,0 +1,14 @@
+-- data type
+-- number
+-- INT, DECIMAL
+CREATE TABLE MyTable3
+(
+ Col1 INT,
+ Col2 DECIMAL
+);
+
+
+-- char(String)
+
+-- date, datetime
+
