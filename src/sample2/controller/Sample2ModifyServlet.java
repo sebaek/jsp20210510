b43bb07aa37f85f61a8a547c8d76cec87b202abc
sebaek <sebaek@gmail.com>
2021-06-03 10:23:15 +0900

    modify

---
+++ src/sample2/controller/Sample2ModifyServlet.java
@@ -0,0 +1,79 @@
+package sample2.controller;
+
+import java.io.IOException;
+import java.sql.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import sample2.bean.Member;
+import sample2.dao.MemberDao;
+
+/**
+ * Servlet implementation class Sample2ModifyServlet
+ */
+@WebServlet("/sample2/modify")
+public class Sample2ModifyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Sample2ModifyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		String name = request.getParameter("name");
+		String birth = request.getParameter("birth");
+		
+		Member member = new Member();
+		member.setId(id);
+		member.setPassword(password);
+		member.setName(name);
+		member.setBirth(Date.valueOf(birth));
+		
+		MemberDao dao = new MemberDao();
+		boolean ok = dao.update(member);
+		
+		String message = "";
+		if (ok) {
+			message = "변경 완료";
+		} else {
+			message = "변경 실패";
+		}
+		
+		request.setAttribute("message", message);
+		
+		String path = "/WEB-INF/sample2/info.jsp";
+		request.getRequestDispatcher(path).forward(request, response);
+	}
+
+}
+
+
+
+
+
+
+
+
