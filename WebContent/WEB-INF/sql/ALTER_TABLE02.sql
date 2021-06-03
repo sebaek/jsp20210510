7dc0097e41d7734ccf57c61d6a6642dadbb030b5
sebaek <sebaek@gmail.com>
2021-06-03 15:45:26 +0900

    alter table

--- WebContent/WEB-INF/sql/ALTER_TABLE02.sql
+++ WebContent/WEB-INF/sql/ALTER_TABLE02.sql
@@ -3,3 +3,34 @@ DESC Member;
 
 ALTER TABLE Member
 ADD PRIMARY KEY(id);
+
+ALTER TABLE Member
+MODIFY COLUMN password varchar(255) NOT NULL;
+
+ALTER TABLE Member
+MODIFY COLUMN name varchar(255) NOT NULL;
+
+ALTER TABLE Member
+MODIFY COLUMN birth DATE NOT NULL;
+
+ALTER TABLE Member
+MODIFY COLUMN inserted TIMESTAMP NOT NULL DEFAULT now();
+
+INSERT INTO Member
+(id, password, name, birth)
+VALUES
+('donald', 'trump', 'trump', '2011-01-01');
+
+SELECT * FROM Member;
+
+
+
+
+
+
+
+
+
+
+
+
