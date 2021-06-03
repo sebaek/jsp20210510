8135a60fae09e3232651a2bd98116da2e1ded9e9
sebaek <sebaek@gmail.com>
2021-06-03 12:11:10 +0900

    not null

---
+++ WebContent/WEB-INF/sql/CONSTRAINT01.sql
@@ -0,0 +1,44 @@
+CREATE TABLE MyTable7
+(
+id INT,
+name VARCHAR(255)
+);
+
+INSERT INTO MyTable7
+(id, name)
+VALUES (3, 'hello');
+
+SELECT * FROM MyTable7;
+
+INSERT INTO MyTable7
+(id) VALUES (4);
+
+INSERT INTO MyTable7
+(name) VALUES ('donald');
+
+-- NOT NULL
+CREATE TABLE MyTable8
+(
+id INT NOT NULL,
+name VARCHAR(255) NOT NULL
+);
+INSERT INTO MyTable8
+(id, name) 
+VALUES (3, 'hello');
+SELECT * FROM MyTable8;
+
+INSERT INTO MyTable8
+(id) VALUES (5); -- x
+
+INSERT INTO MyTable8
+(name) VALUES ('donald'); -- x
+
+
+
+
+
+
+
+
+
+
