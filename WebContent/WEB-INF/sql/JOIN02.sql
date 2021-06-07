036a822843c9bdd8df5f3b233fab477cbdc04234
sebaek <sebaek@gmail.com>
2021-06-07 11:30:27 +0900

    tables

--- WebContent/WEB-INF/sql/JOIN02.sql
+++ WebContent/WEB-INF/sql/JOIN02.sql
@@ -11,3 +11,26 @@ CREATE TABLE Likes (
 	userId INT,
     `like` VARCHAR(255)
 );
+
+INSERT INTO Users (id, name) VALUES (1, 'Patrik');
+INSERT INTO Users (id, name) VALUES (2, 'Albert');
+INSERT INTO Users (id, name) VALUES (3, 'Maria');
+INSERT INTO Users (id, name) VALUES (4, 'Darwin');
+INSERT INTO Users (id, name) VALUES (5, 'Elizabeth');
+
+select * from Users;
+
+INSERT INTO Likes (userId, `like`)  VALUES (3, 'Stars');
+INSERT INTO Likes (userId, `like`)  VALUES (1, 'Climbing');
+INSERT INTO Likes (userId, `like`)  VALUES (1, 'Code');
+INSERT INTO Likes (userId, `like`)  VALUES (6, 'Rugby');
+INSERT INTO Likes (userId, `like`)  VALUES (4, 'Apples');
+
+
+
+
+
+
+
+
+
