cdc7a43718fa5e42bfcedef184c88859c3142cc0
sebaek <sebaek@gmail.com>
2021-06-07 12:32:42 +0900

    join 3 tablkes

---
+++ WebContent/WEB-INF/sql/JOIN05.sql
@@ -0,0 +1,17 @@
+USE test;
+
+SELECT * FROM Orders;
+
+-- 주문의 고객명
+SELECT o.OrderID, c.CustomerName 
+FROM Orders o JOIN Customers c 
+ON o.CustomerID = c.CustomerID;
+
+-- 주문의 고객명, 직원명
+SELECT o.OrderID, c.CustomerName, e.LastName, e.FirstName
+FROM Orders o 
+JOIN Customers c ON o.CustomerID = c.CustomerID
+JOIN Employees e ON o.EmployeeID = e.EmployeeID
+;
+
+-- desc Employees;
