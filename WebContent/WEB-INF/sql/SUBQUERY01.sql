dd6300f1101c427746f89eae067e8fd728764374
sebaek <sebaek@gmail.com>
2021-06-07 14:37:51 +0900

    subquery

--- WebContent/WEB-INF/sql/SUBQUERY01.sql
+++ WebContent/WEB-INF/sql/SUBQUERY01.sql
@@ -15,6 +15,9 @@ SELECT * FROM Customers
 WHERE Country = (SELECT Country FROM Customers WHERE CustomerID = 1);
 
 -- 1번고객(알프레드)와 같은 나라에 있는 공급자(Suppliers)목록
+SELECT * FROM Suppliers
+WHERE Country = (SELECT Country FROM Customers WHERE CustomerID = 1);
+
 
 
 
