07bd8380d1efc4bca9d07100798ba7a3b86fe192
sebaek <sebaek@gmail.com>
2021-06-07 15:31:54 +0900

    any

---
+++ WebContent/WEB-INF/sql/SUBQUERY03.sql
@@ -0,0 +1,19 @@
+USE test;
+
+SELECT ProductName
+FROM Products
+WHERE ProductID = ANY
+  (SELECT ProductID
+  FROM OrderDetails
+  WHERE Quantity = 10);
+  
+SELECT ProductID
+FROM OrderDetails
+WHERE Quantity = 10;
+
+SELECT * FROM OrderDetails;
+
+
+
+
+
