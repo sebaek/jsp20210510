70dbda35b39691bccc1c0cc4ea15917902f73d25
sebaek <sebaek@gmail.com>
2021-06-07 12:21:21 +0900

    join

--- WebContent/WEB-INF/sql/JOIN04.sql
+++ WebContent/WEB-INF/sql/JOIN04.sql
@@ -1,4 +1,8 @@
 USE test2;
 
--- 게시물을 작성하지 않은 Member의 id를 나열해
+-- 게시물을 작성하지 않은 Member의 id, name 를 나열해
+SELECT m.id , m.name 
+FROM Board b RIGHT JOIN Member m
+ON b.memberId = m.id
+WHERE b.memberId IS NULL;
 
