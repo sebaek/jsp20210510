fb3cc0339c8597efe6df0c02fdfad54272f248f0
sebaek <sebaek@gmail.com>
2021-06-07 15:22:46 +0900

    in, not in

--- WebContent/WEB-INF/sql/SUBQUERY02.sql
+++ WebContent/WEB-INF/sql/SUBQUERY02.sql
@@ -15,8 +15,8 @@ SELECT * FROM Customers WHERE CustomerID NOT IN (SELECT CustomerID FROM Orders);
 DESC Orders;
 
 -- 주문을 처리하는 직원 목록
-
+SELECT * FROM Employees WHERE EmployeeID IN (SELECT EmployeeID FROM Orders);
 -- 주문을 처리하지 않는 직원 목록
-
+SELECT * FROM Employees WHERE EmployeeID NOT IN (SELECT EmployeeID FROM Orders);
 
 
