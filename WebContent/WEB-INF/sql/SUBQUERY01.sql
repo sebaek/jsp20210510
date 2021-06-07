b109343ba129de6eecaba8f609039ed2d7489ff7
sebaek <sebaek@gmail.com>
2021-06-07 15:11:36 +0900

    subquery

--- WebContent/WEB-INF/sql/SUBQUERY01.sql
+++ WebContent/WEB-INF/sql/SUBQUERY01.sql
@@ -18,6 +18,23 @@ WHERE Country = (SELECT Country FROM Customers WHERE CustomerID = 1);
 SELECT * FROM Suppliers
 WHERE Country = (SELECT Country FROM Customers WHERE CustomerID = 1);
 
+-- 1번고객(알프레드)와 다른 나라에 사는 고객들 목록
+SELECT * FROM Customers
+WHERE Country <> (SELECT Country FROM Customers WHERE CustomerID = 1);
+
+SELECT * FROM Customers
+WHERE Country != (SELECT Country FROM Customers WHERE CustomerID = 1);
+
+-- 
+DESC Employees;
+SELECT * FROM Employees;
+
+-- 직원 Fuller(2) 보다 나이가 많은 사람
+SELECT * FROM Employees
+WHERE BirthDate < '1952-02-19';
+
+SELECT * FROM Employees
+WHERE BirthDate < (SELECT BirthDate FROM Employees WHERE EmployeeID = 2);
 
 
 
