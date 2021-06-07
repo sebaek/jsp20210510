10c7357168f3a4e2d46b5252dbe37e6703e86ae2
sebaek <sebaek@gmail.com>
2021-06-07 14:31:24 +0900

    subquery

--- WebContent/WEB-INF/sql/SUBQUERY01.sql
+++ WebContent/WEB-INF/sql/SUBQUERY01.sql
@@ -1,2 +1,23 @@
 USE test;
 
+-- 1번 고객(알프레드)의 나라
+SELECT Country FROM Customers
+WHERE CustomerID = 1;
+-- Germany
+
+-- 
+SELECT * FROM Customers
+WHERE Country = 'Germany';
+
+
+-- subquery
+SELECT * FROM Customers
+WHERE Country = (SELECT Country FROM Customers WHERE CustomerID = 1);
+
+
+
+
+
+
+
+
