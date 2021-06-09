93c3e3b9326b825419ab599fbcfc5b9c6a00f600
sebaek <sebaek@gmail.com>
2021-06-09 09:22:55 +0900

    comment table

---
+++ WebContent/WEB-INF/sql/CREATE_TABLE05_Comment_CRUD.sql
@@ -0,0 +1,12 @@
+USE test2;
+DROP TABLE Comment;
+CREATE TABLE Comment
+(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	comment VARCHAR(2047) NOT NULL,
+    memberId VARCHAR(255) NOT NULL,
+    boardId INT NOT NULL,
+    inserted TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (memberId) REFERENCES Member(id),
+    FOREIGN KEY (boardId) REFERENCES Board(id)
+);
