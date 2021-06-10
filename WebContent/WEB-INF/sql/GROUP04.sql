c8e9fd378acc31af1f296bf23c0fdd492dd81d89
sebaek <sebaek@gmail.com>
2021-06-10 11:09:49 +0900

    board list

--- WebContent/WEB-INF/sql/GROUP04.sql
+++ WebContent/WEB-INF/sql/GROUP04.sql
@@ -14,3 +14,21 @@ GROUP BY b.id
 ORDER BY b.id DESC;
 
 
+SELECT b.id boardId,
+       b.title title,
+       m.name name,
+       count(c.id) numberOfComment,
+       b.inserted
+FROM Board b
+JOIN Member m
+ON b.memberId = m.id
+LEFT JOIN Comment c
+ON b.id = c.boardId
+GROUP BY b.id
+ORDER BY boardId DESC
+
+
+
+
+
+
