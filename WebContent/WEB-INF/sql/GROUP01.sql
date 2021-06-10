147e31c0955f83118dfc28c17fa180e9b7737376
sebaek <sebaek@gmail.com>
2021-06-10 09:37:15 +0900

    group function

--- WebContent/WEB-INF/sql/GROUP01.sql
+++ WebContent/WEB-INF/sql/GROUP01.sql
@@ -20,3 +20,46 @@ SELECT sum(Price) FROM Products;
 SELECT * FROM Products
 ORDER BY Price ;
 
+-- int?
+CREATE TABLE Test1 (
+	col1 INT,
+    col2 DECIMAL(10, 2),
+    col3 VARCHAR(255)
+);
+
+INSERT INTO Test1 (col1, col2, col3) VALUES (1, 3.14, '3.14');
+INSERT INTO Test1 (col1, col2, col3) VALUES (1, 3.14, '삼쩜일사');
+
+SELECT * FROM Test1;
+
+SELECT avg(col1) FROM Test1;
+SELECT avg(col2) FROM Test1;
+SELECT avg(col3) FROM Test1;
+
+
+-- exercise01
+SELECT 
+MIN(Price) minPrice
+FROM Products;
+
+-- exercise02
+SELECT 
+max(Price) AS maxPrice
+FROM Products;
+
+-- exercise03
+SELECT * FROM Products ORDER BY Price;
+SELECT COUNT(*) FROM Products WHERE Price = 18;
+
+-- exercise04
+SELECT AVG(Price) FROM Products;
+
+-- exercise05
+SELECT SUM(Price) FROM Products;
+
+
+
+
+
+
+
