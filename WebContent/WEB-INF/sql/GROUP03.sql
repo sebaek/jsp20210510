e0ace190945d5d8bfa96646f32a910bf948502f7
sebaek <sebaek@gmail.com>
2021-06-10 10:31:54 +0900

    having

---
+++ WebContent/WEB-INF/sql/GROUP03.sql
@@ -0,0 +1,20 @@
+USE test;
+
+SELECT Country, COUNT(CustomerId)
+FROM Customers
+GROUP BY Country
+HAVING COUNT(CustomerID) > 5;
+
+SELECT Employees.LastName, COUNT(Orders.OrderID) AS NumberOfOrders
+FROM (Orders
+INNER JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID)
+GROUP BY LastName
+HAVING COUNT(Orders.OrderID) > 10;
+
+SELECT Employees.LastName, COUNT(Orders.OrderID) AS NumberOfOrders
+FROM Orders
+INNER JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+WHERE LastName = 'Davolio' OR LastName = 'Fuller'
+GROUP BY LastName
+HAVING COUNT(Orders.OrderID) > 25;
+
