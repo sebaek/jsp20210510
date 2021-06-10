e960e63104e1c460e7eb60207540bce608e85744
sebaek <sebaek@gmail.com>
2021-06-10 10:22:15 +0900

    group by

--- WebContent/WEB-INF/sql/GROUP02.sql
+++ WebContent/WEB-INF/sql/GROUP02.sql
@@ -15,3 +15,20 @@ SELECT Country, COUNT(*) cnt
 FROM Customers
 GROUP BY Country
 ORDER BY cnt DESC;
+
+-- exercise 01
+SELECT 
+COUNT(CustomerID),
+Country
+FROM Customers
+GROUP BY Country
+;
+
+-- exercise 02
+SELECT 
+count(CustomerID),
+Country
+FROM Customers
+GROUP BY Country
+ORDER BY 
+count(CustomerID) DESC;
